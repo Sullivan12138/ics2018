a3166d1a5367cdac50bd6dcc4b8523662a182545
tracer-ics2018 <tracer@njuics.org>
2019-12-10 15:05:31 +0800

    >  compile U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:05:31 up  5:57,  1 user,  load average: 0.12, 0.10, 0.08 d3286d5d1d42eee14c38735d856874d790f39114

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+static int cmd_si(){}
 static struct {
   char *name;
   char *description;
@@ -46,7 +46,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si [N]", "let the procedure execute N instructions step by step", cmd_si},
   /* TODO: Add more commands */
 
 };
