40bcaa67c6167de14df4dc7395da52b824352f08
tracer-ics2018 <tracer@njuics.org>
2019-12-12 11:30:56 +0800

    >  compile U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:30:56 up 16:40,  1 user,  load average: 0.29, 0.13, 0.09 32e499d4c0beba216afc774e52bc16b2832d22f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -98,6 +98,7 @@ static bool make_token(char *e) {
                   nr_token++;
                   break;
               };
+              case TK_NOTYPE: break;
               default: tokens[nr_token++].type = rules[i].token_type;
           }
 
