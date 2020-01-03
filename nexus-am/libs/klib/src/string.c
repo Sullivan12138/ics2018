ef901d26acafa8469b83348906f0246f3042f381
TaoWei <ericsullivan538@gmail.com>
2020-01-03 14:54:42 +0800

    x

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,85 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t i;
+  for(i = 0; s[i] != '\0';) i++;
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  return strncpy(dst, src, strlen(src));
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+  for(i = 0; src[i] != '\0' && i < n; i++) dst[i] = src[i]; 
+  dst[i] = '\0';
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t i = strlen(dst);
+  size_t j;
+  for(j = 0; src[j] != '\0'; j++) {
+    dst[i+j] = src[j];
+  }
+  dst[i+j] = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
+  while(*s1 && *s2) {
+    if(*s1 > *s2) return 1;
+    else if(*s1 < *s2) return -1;
+    s1++;
+    s2++;
+  }
+  if(*s1) return 1;
+  else if(*s2) return -1;
   return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  size_t i = 0;
+  while(*s1 && *s2) {
+    if(*s1 > *s2) return 1;
+    else if(*s1 < *s2) return -1;
+    i++;
+    if(i == n) break;
+    s1++;
+    s2++;
+  }
+  if(*s1) return 1;
+  else if(*s2) return -1;
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  while(n != 0) {
+    (*(char*)v) = c;
+    v++;
+    n--;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  void *head = out;
+  while(n != 0) {
+    (*(char*)out) = (*(char*)in);
+    in++;
+    out++;
+    n--;
+  }
+  return head;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  while(n != 0) {
+    if(*(char*)s1 > *(char*)s2) return 1;
+    else if(*(char*)s1 < *(char*)s2) return -1;
+    n--;
+  }
   return 0;
 }
 
