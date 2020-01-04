ae0d0f0181bf9ac1214da7af0dc361b8ffc9d552
tracer-ics2018 <tracer@njuics.org>
2020-01-04 20:16:29 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:16:29 up 1 day, 17:20,  1 user,  load average: 0.25, 0.24, 0.19 e6e32c33b6ef466d0e9525f56de024a8b5b85d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
 #include <am.h>
 
 uintptr_t sys_yield() {
@@ -11,6 +12,16 @@ void sys_exit(int code) {
   _halt(code);
 }
 
+
+size_t sys_write(int fd, void *buf, size_t count) {
+  if(fd == 1 || fd == 2) {
+    char *str = buf;
+    int i = 0;
+    for(;i< count; i++) _putc(*(str+i));
+  }
+  return count;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -18,8 +29,9 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    case SYS_yield: c->GPRx = sys_yield();
+    case SYS_yield: c->GPRx = sys_yield(); break;
     case SYS_exit: sys_exit(a[1]); break;
+    case SYS_write: c->GPRx = sys_write(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
