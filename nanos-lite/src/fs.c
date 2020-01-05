7968b171bfd5743a0261d68495dd0309fb9c9b5f
tracer-ics2018 <tracer@njuics.org>
2020-01-05 20:29:22 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:29:22 up 1 day, 23:11,  1 user,  load average: 0.88, 0.56, 0.33 48e96d0d534102803afa3e1548b18ec426cf8186

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,7 +68,7 @@ size_t fs_filesz(int fd) {
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len) {
-  printf("fs_read:%d\n", fd);
+  // printf("fs_read:%d\n", fd);
   size_t l = (file_table[fd].open_offset + len) <= fs_filesz(fd) ? len : (fs_filesz(fd) - file_table[fd].open_offset);
   if(file_table[fd].read == NULL) ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   else l = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
