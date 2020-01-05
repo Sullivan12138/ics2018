892030ac6948bd41409d126da32f144b8c6ed223
tracer-ics2018 <tracer@njuics.org>
2020-01-05 20:14:40 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:14:40 up 1 day, 22:56,  1 user,  load average: 0.28, 0.20, 0.13 e6a428f8cdd87630498d4bc73b658d587aa8e626

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,7 +2,11 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  size_t i;
+  for (i = 0; i < len; ++i) {
+    _putc(*((char *)buf + i));
+  }
+  return i;
 }
 
 #define NAME(key) \
@@ -20,11 +24,17 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  strncpy(buf, dispinfo + offset, len);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  int x = (offset / 4) % screen_width();
+  int y = (offset / 4) / screen_width();
+  draw_sync();
+  draw_rect((uint32_t* )buf, x, y, len / 4, 1);
+  
+  return len;
 }
 
 void init_device() {
@@ -33,4 +43,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
