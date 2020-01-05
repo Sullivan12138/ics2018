1e37ac97e303d87be61899999e90373d73497736
tracer-ics2018 <tracer@njuics.org>
2020-01-05 19:48:03 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:48:03 up 1 day, 22:30,  1 user,  load average: 0.41, 0.28, 0.18 d423505bc5a566691e4c3754e91e72b8b2333ebf

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,21 +4,25 @@
 #define MAP_TEST 1
 #define MAP_CREATE 2
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  // int fd = fs_open(filename, 0, 0);
+  // int len = fs_filesz(fd);
+  // int blen = pcb->as.pgsize;
+  // uintptr_t s = DEFAULT_ENTRY;
+  // char buf[blen];
+  // while(len > 0) {
+  //   void *page_base = new_page(1);
+  //   _map(&pcb->as, (void *)s, page_base, MAP_CREATE);
+  //   fs_read(fd, buf, blen);
+  //   memcpy(page_base, buf, blen);
+  //   s += blen;
+  //   len -= blen;
+  // }
+  // pcb->cur_brk = pcb->max_brk = s;
+  // fs_close(fd);
   int fd = fs_open(filename, 0, 0);
   int len = fs_filesz(fd);
-  int blen = pcb->as.pgsize;
-  uintptr_t s = DEFAULT_ENTRY;
-  char buf[blen];
-  while(len > 0) {
-    void *page_base = new_page(1);
-    _map(&pcb->as, (void *)s, page_base, MAP_CREATE);
-    fs_read(fd, buf, blen);
-    memcpy(page_base, buf, blen);
-    s += blen;
-    len -= blen;
-  }
-  pcb->cur_brk = pcb->max_brk = s;
-  fs_close(fd);
+  char buf[len];
+  fs_read(fd, buf, len);
   return DEFAULT_ENTRY;
 }
 
