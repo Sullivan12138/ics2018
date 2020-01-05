0f3cb415bf5c8995249bd07876d7d6dc4614343f
TaoWei <ericsullivan538@gmail.com>
2020-01-05 18:24:51 +0800

    x

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -6,6 +6,14 @@
 #ifndef SEEK_SET
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 #endif
+size_t invalid_read(void *buf, size_t offset, size_t len);
+size_t invalid_write(const void *buf, size_t offset, size_t len);
 
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
+size_t fs_filesz(int fd);
 
 #endif
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,24 @@
 #include "proc.h"
+#include "fs.h"
 #define DEFAULT_ENTRY 0x4000000
-
+#define MAP_TEST 1
+#define MAP_CREATE 2
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  ramdisk_read((void*)DEFAULT_ENTRY, 0, get_ramdisk_size());
+  int fd = fs_open(filename, 0, 0);
+  int len = fs_filesz(fd);
+  int blen = pcb->as.pgsize;
+  uintptr_t s = DEFAULT_ENTRY;
+  char buf[blen];
+  while(len > 0) {
+    void *page_base = new_page(1);
+    _map(&pcb->as, (void *)s, page_base, MAP_CREATE);
+    fs_read(fd, buf, blen);
+    memcpy(page_base, buf, blen);
+    s += blen;
+    len -= blen;
+  }
+  pcb->cur_brk = pcb->max_brk = s;
+  fs_close(fd);
   return DEFAULT_ENTRY;
 }
 
