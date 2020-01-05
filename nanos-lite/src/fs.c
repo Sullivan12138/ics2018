4c8b949350e9cff44019d24eb90f4e5e269f7a27
tracer-ics2018 <tracer@njuics.org>
2020-01-05 22:39:38 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:39:38 up 2 days,  1:21,  1 user,  load average: 0.60, 0.35, 0.29 469179df1c473944276e45924b89b4c0971f6e83

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -87,13 +87,14 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
-  printf("lseek: %d\n", fd);
+  printf("lseek: %d, offset: %d\n", fd, offset);
   switch(whence) {
     case SEEK_SET: assert(offset <= file_table[fd].size); file_table[fd].open_offset = offset; break;
     case SEEK_CUR: assert(file_table[fd].open_offset + offset <= file_table[fd].size); file_table[fd].open_offset += offset; break;
     case SEEK_END:  file_table[fd].open_offset = file_table[fd].size + offset; break;
     default: panic("Unkown whence.\n");
   }
+  
   return file_table[fd].open_offset;
 }
 
