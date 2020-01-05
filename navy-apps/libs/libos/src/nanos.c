8e526bb3e624b05bed3f6044959a86113c222a19
tracer-ics2018 <tracer@njuics.org>
2020-01-05 22:18:17 +0800

    >  run U201613626 hust Linux hust-desktop 5.3.0-24-generic #26-Ubuntu SMP Thu Nov 14 01:33:18 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:18:17 up 2 days,  1:00,  1 user,  load average: 0.41, 0.34, 0.29 3016d966d7cd6c7a5235d3efb5e02e1165d779f8

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -47,8 +47,7 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _syscall_(SYS_read, fd, buf, count);
-  return 0;
+  return _syscall_(SYS_read, fd, buf, count);
 }
 
 int _close(int fd) {
